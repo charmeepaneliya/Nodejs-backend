6f91d546221e358a69075e149f7f0d7aa758c188
Charmee Paneliya <cpjstudy28@gmail.com>
2026-05-19 14:13:50 +0530

    add update and delete operations

--- 07_student_management_system/controller/studentController.js
+++ 07_student_management_system/controller/studentController.js
@@ -4,6 +4,8 @@ import HttpError from "../middleware/httpError.js";
 
 import Student from "../model/Student.js";
 
+//create student data
+
 const add = async (req,res,next)=>{
     try{
         const { name,grId,email,course,isActive,mobileNumber } = req.body;
@@ -21,6 +23,8 @@ const add = async (req,res,next)=>{
     
 };
 
+//read student data
+
 const getAllStudentData = async (req,res,next)=>{
     try{
         const students = await Student.find({});
@@ -34,4 +38,60 @@ const getAllStudentData = async (req,res,next)=>{
     }
 };
 
-export default {add,getAllStudentData};
+//get student by id
+
+const getStudentdById = async (req,res,next)=>{
+    try{
+        const {id} = req.params;
+
+        const student = await Student.findById(id);
+
+        if(!student){
+            return next(new HttpError("student not found with this id",404));
+        }
+
+        res.status(200).json({success:true,message:"student data fetched successfully",student});
+    }catch (error){
+        next(new HttpError(error.message,500));
+    }
+};
+
+//update student data
+
+const updateStudentData = async (req,res,next)=>{
+     try{
+        const {id} = req.params;
+
+        const updatedStudent = await Student.findByIdAndUpdate(
+            id,
+            req.body,
+            {new:true}
+        );
+
+        if(!updatedStudent){
+            return next(new HttpError("student not found with this id",404));
+        }
+         res.status(200).json({success:true,message:"student data updated successfully",updatedStudent});
+    }catch (error){
+        next(new HttpError(error.message,500));
+    }
+}
+
+//delete student data
+
+const deleteStudentData = async (req,res,next)=>{
+     try{
+        const {id} = req.params;
+
+        const deleteStudent = await Student.findByIdAndDelete(id);
+
+        if(!deleteStudent){
+            return next(new HttpError("student not found with thid id",404));
+        }
+         res.status(200).json({success:true,message:"student data deleted successfully"});
+    }catch (error){
+        next(new HttpError(error.message,500));
+    }
+}
+
+export default {add,getAllStudentData,getStudentdById,deleteStudentData,updateStudentData};
--- 07_student_management_system/routes/studentRoutes.js
+++ 07_student_management_system/routes/studentRoutes.js
@@ -9,4 +9,10 @@ router.post("/add",studentController.add);
 
 router.get("/getAllStudents",studentController.getAllStudentData);
 
+router.get("/:id",studentController.getStudentdById);
+
+router.patch("/:id",studentController.updateStudentData);
+
+router.delete("/:id",studentController.deleteStudentData);
+
 export default router;
